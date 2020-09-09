031389a8f890ee19ddace4e72b8aff1e986c90aa
Santino <sbonarrigo@udesa.edu.ar>
2020-09-09 13:58:13 -0300

    rutas terminadas

--- proyecto-integrador/app.js
+++ proyecto-integrador/app.js
@@ -3,11 +3,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var agregarPostRouter = require("./routes/agregarPost")
-var detallePostRouter = require("./routes/detallepost")
-var detalleusuarioRouter = require("./routes/detalleUsuario.js")
+let indexRouter = require('./routes/index');
+let agregarPostRouter = require("./routes/agregarPost")
+let detallePostRouter = require("./routes/detallepost")
+let detalleusuarioRouter = require("./routes/detalleUsuario")
+let headerlogueadoRouter = require("./routes/headerlogueado")
+let headerdeslogueadoRouter = require("./routes/headerDeslogueado")
+let footerRouter = require("./routes/footer")
+let homeRouter = require("./routes/home")
+let loginRouter = require("./routes/login")
+let miperfilRouter = require("./routes/miperfil")
+let registracionRouter = require("./routes/registracion")
+let resultadobusquedaRouter = require ("./routes/resultadobusqueda")
+
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -18,11 +26,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/agregarpost', agregarPostRouter );
 app.use('/detallepost', detallePostRouter );
 app.use('/detalleusuario', detalleusuarioRouter);
-
-
-
+app.use("/headerlogueado",headerlogueadoRouter)
+app.use("/headerdeslogueado",headerdeslogueadoRouter)
+app.use("/footer", footerRouter)
+app.use("/home", homeRouter)
+app.use("/login", loginRouter)
+app.use("/perfil", miperfilRouter)
+app.use("/registracion",registracionRouter)
+app.use("/resultadobusqueda", resultadobusquedaRouter)
 module.exports = app;
