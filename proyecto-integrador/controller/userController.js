69d8a5814bac495eaf2667cd48914c192e32ee68
David <davidzir.2001@gmail.com>
2020-10-28 16:28:10 -0300

    session y cookies instalado

--- proyecto-integrador/controller/userController.js
+++ proyecto-integrador/controller/userController.js
@@ -1,3 +1,9 @@
+// const bcrypt = require('bcryptjs');
+// const db = require('../db/models');
+// const users = db.User;
+
+// const op = db.Sequelize.Op;
+
 let userController = {
     
     miPerfil: function(req, res) {
@@ -22,7 +28,31 @@ let userController = {
         
         res.render("login")
 
+       //encontrar el email
+       //Chequear que la contraseña coincida.
+    //    users.findOne({
+    //        where: [{ email : req.body.email }]
+    //    })
+    //     .then( function(user){
+    //         //El email no está en la base de datos
+    //         if(user == null){
+    //             return res.send("Email incorrecto");
+    //         } else if (bcrypt.compareSync(req.body.password, user.password) == false ){
+    //             //EL email existe pero la contraseña está mal
+    //             return res.send("Contraseña equivocada")
+    //         } else if (bcrypt.compareSync(req.body.password, user.password)){
+    //             //Coinciden las contraseñas
+    //             req.session.user = user
+    //             return res.redirect('/');
+    //         }
+
+    //     })
+    //     .catch( e => console.log(e))
+
     },
+
+
+
 
     home: function(req, res) {
         
